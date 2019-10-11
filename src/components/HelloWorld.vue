9b8af4ff21c484110173d00c954240b1a079a5e3
katasu-git <katasu9923@gmail.com>
2019-10-11 22:12:54 +0900

    guide用のトップページのhtmlを書き終わった

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,130 @@
 <template>
-  <div class="hello">
-    <button v-on:click="accessDb()">hi!</button>
+  <div id="hello">
+    <div class="l-body">
+      <div class="l-logo">
+        <div class="o-logo-text">
+          <div class="o-logo-text-l">DoCoGeo</div>
+          <div class="o-logo-text-s">for Guide</div>
+        </div>
+        <div class="o-logo-img">
+          <img src="../assets/duck.svg" />
+        </div>
+      </div>
+      <div class="l-justify-center">
+        <div class="l-cardContainer">
+          <div class="o-card" v-for="info in tour_info">
+            <div class="o-card-img"></div>
+            <div class="o-card-title">{{ info.tour_name }}</div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: "hello!"
-    }
-  },
-  methods: {
-    accessDb: function() {
+  import axios from 'axios'
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        msg: "hello!",
+        numOfCard: 5,
+        tour_info: JSON,
+      }
+    },
+    created: function () {
+      this.accessDb();
+    },
+    methods: {
+      accessDb: function () {
         axios.post('https://www2.yoslab.net/~nishimura/geotour/PHP/api.php').then(response => {
-        console.log(response.data.tour_name);
-        console.log(response.data.group_name);
-      }).catch(error => {
-        // エラーを受け取る
-        console.log(error);
-      });
+          this.tour_info = response.data;
+          console.log(this.tour_info);
+          //console.log(response.data);
+        }).catch(error => {
+          // エラーを受け取る
+          console.log(error);
+        });
+      }
     }
   }
-}
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  #hello,
+  .l-body {
+    height: 100%;
+    width: 100%;
+  }
+
+  .l-body {
+    background: #5c9982;
+  }
+
+  .l-justify-center {
+    display: flex;
+    justify-content: center;
+    /*中央よせ*/
+  }
+
+  .l-cardContainer {
+    width: calc(100% - 40px);
+
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    /*折り返しの指定*/
+  }
+
+  .o-card {
+    min-height: calc(50vw - 25px);
+    min-width: calc(50vw - 25px);
+
+    border: solid 0 white;
+    border-radius: 3px;
+    background: white;
+  }
+
+  .o-card:nth-child(n+3) {
+    margin-top: 10px;
+  }
+
+  .o-card-img {
+    height: calc( (50vw - 25px)/5*3 );
+    width: 100%;
+
+    background-color: #c9c9c9;
+  }
+
+  .o-card-title {
+    font-size: 12px;
+    /*font-weight: bold;*/
+  }
+
+  .l-logo {
+    padding: 20px;
+    display: flex;
+  }
+
+  .o-logo-text {
+    color: white;
+  }
+
+  .o-logo-text-l {
+    font-size: 36px;
+    font-weight: bold;
+  }
+
+  .o-logo-text-s {
+    font-size: 12px;
+    font-weight: bold;
+  }
+
+  .o-logo-img>img {
+    height: 60px;
+  }
+
 </style>
