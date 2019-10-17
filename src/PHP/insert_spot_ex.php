46a8c805ea8a3752c903821d9fc706d8981f049f
katasu-git <katasu9923@gmail.com>
2019-10-17 15:30:19 +0900

    innert PHPのバグを修正

--- src/PHP/insert_spot_ex.php
+++ src/PHP/insert_spot_ex.php
@@ -10,8 +10,8 @@ function innsert_spot_ex() {
     $pdo = connect_mysql();  //mysqlに接続
 
     $stmt = $pdo -> prepare("INSERT INTO 
-    spot_explanation (tour_id, spot_id, spot_name, spot_explanation) 
-    VALUES (:tour_id, :spot_id, :spot_name, :spot_explanation)");
+    spot_explanation (tour_id, spot_id, spot_explanation) 
+    VALUES (:tour_id, :spot_id, :spot_explanation)");
     $stmt->bindValue(':tour_id', $tour_id, PDO::PARAM_INT);
     $stmt->bindValue(':spot_id', $spot_id, PDO::PARAM_INT);
     $stmt->bindParam(':spot_explanation', $spot_ex, PDO::PARAM_STR);
