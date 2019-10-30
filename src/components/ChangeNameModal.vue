53fd3dbab1417157c2e9bf2f67bef548aca35cf4
katasu-git <katasu9923@gmail.com>
2019-10-30 11:58:18 +0900

    スポット名変更時のモーダルも変更

--- src/components/ChangeNameModal.vue
+++ src/components/ChangeNameModal.vue
@@ -2,10 +2,15 @@
   <div id="chageNameModal">
     <div class="l-body">
         <form class="l-input_ex">
-            <input class="o-input_ex" type="text" placeholder="ジオサイトの名前を入力" v-model="spot_name_updated" />
+            <input class="o-input_ex" type="text" placeholder="スポットの名前を入力" v-model="spot_name_updated" />
         </form>
-        <button v-on:click="changeName()">保存する</button>
-        <button v-on:click="closeModal()">キャンセル</button>
+        <div class="l-err_message">
+          <div class="o-err_message" v-show="errFlag()">名前を入力してください！</div>
+        </div>
+        <div class="l-button">
+            <button class="o-btn-prime" v-on:click='changeName()'>保存する</button>
+            <button class="o-btn-second u-mt16px" v-on:click='closeModal()'>キャンセル</button>
+        </div>
     </div>
   </div>
 </template>
@@ -21,6 +26,7 @@
     data() {
       return {
           spot_name_updated: '',
+          errFlag_first: false,
       }
     },
     created: function () {
@@ -28,6 +34,10 @@
     },
     methods: {
         changeName: function() {
+          if (this.spot_name_updated == '') {
+                this.errFlag_first = true;
+                return;
+          }
         const url =
         "https://www2.yoslab.net/~nishimura/geotour/PHP/update_spot_name.php";
         let params = new URLSearchParams();
@@ -48,6 +58,11 @@
         },
         closeModal: function() {
             this.$emit('closeModal');
+        },
+        errFlag() {
+          if (this.errFlag_first && this.spot_name_updated == '') {
+                return true;
+          }
         }
     }
   }
@@ -67,16 +82,77 @@
 }
 
 .l-input_ex {
-    padding: 50px 0 0 20px;
+  width: 100vw;
+  padding-top: 50px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
 .o-input_ex {
-    min-height: 30px;
-    width: calc(100vw - 80px);
+    min-height: 60px;
+    width: calc(100vw - 40px);
     border-radius: 5px;
+    font-size: 16px;
+    background: rgba(255, 255, 255, .7);
 }
+
 button {
     color: white;
 }
 
+.u-mt16px {
+    margin-top: 16px;
+}
+
+.l-button {
+    width: 100%;
+
+    padding-top: 40px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.o-btn-prime {
+    height: 60px;
+    width: calc(100vw - 40px);
+    border: solid 0 white;
+    border-radius: 100px;
+    background-color: #F7941E;
+
+    font-size: 20px;
+    font-weight: bold;
+}
+
+.o-btn-second {
+    height: 60px;
+    width: calc(100vw - 40px);
+    border: solid 2px white;
+    border-radius: 100px;
+    background-color: rgba(0,0,0,0);
+
+    font-size: 20px;
+    font-weight: bold;
+}
+
+.l-text-title {
+    padding: 40px 0 0 20px;
+}
+
+.o-text-title {
+  font-size: 20px;
+  font-weight: bold;
+  color: white;
+}
+
+.l-err_message {
+  padding: 20px 0 0 20px;
+}
+
+.o-err_message {
+  color: red;
+}
 </style>
